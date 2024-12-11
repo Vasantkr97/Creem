e222ea5a6541f3760899f36aeca926c22078a6db
Vasantkr97 <vasantkr97@gmail.com>
2024-12-11 20:36:25 +0530

    fixed index

--- src/components/FiveRows.tsx
+++ src/components/FiveRows.tsx
@@ -6,7 +6,7 @@ const FiveRows = () => {
   return (
     <div className="flex flex-col space-y-4 p-4">
       { enableConstants.map((item, index) => (
-        <div className="flex justify-between items-center">
+        <div key={index} className="flex justify-between items-center">
           <div className="flex space-x-2 items-center">
               <p className="px-3 py-0.5 rounded-xl bg-primary/20 text-xs">{item.text}</p>
               <p className="text-xs">{item.subtext}</p>
